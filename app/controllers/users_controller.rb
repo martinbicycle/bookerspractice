fb86aac92b64460322f829309243a124a84cad01
martinbicycle <martinbicycle7700@gmail.com>
2021-01-25 13:11:03 +0000

    [Add]practice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
-    
   end
-  
+
   def edit
     @user = User.find(params[:id])
-  end  
-  
-  
+  end
+
+  def  updated
+    @user = current_user
+    @user.update
+    redirect_to user_path
+  end
+
+
 end
